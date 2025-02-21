f4f587bef8358405ca719be71eae17be4626f5f4
Robert Muresan <robotzelu89@gmail.com>
2025-02-21 06:59:47 +0000

    tabla colorida

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '../App.css'
 const NotFound = () => {
   return (
-    <div>
+    <div className='not-found'>
       <img src="https://shop.raynvillesuperstore.co.uk/cdn/shop/files/gmshms_987x.jpg?v=1699455648" alt="Not Found" />
       
     </div>
